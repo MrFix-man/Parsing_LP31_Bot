c899ac8b68b8bfd504faf67f0515aafc59cfb589
Stnislav-Kankin <stanislav.kankin@yandex.ru>
2023-12-15 09:07:32 +0300

    Завернул бота в класс

--- source/bot.py
+++ source/bot.py
@@ -8,32 +8,42 @@ from telegram.ext import (
 from telegram import ReplyKeyboardMarkup, KeyboardButton
 
 
+class Bot:
+
+    """Основные данные бота"""
+    def __init__(self, token: str):
+        self.mybot = Updater(token, use_context=True)
+       
+        
+    """Запуск бота и работа с командами, устанавливаем кнопки"""
+    def main(self):
+        dp = self.mybot.dispatcher
+
+        dp.add_handler(CommandHandler('start', self.hello_user))
+        dp.add_handler(CommandHandler('search', self.search))
+        # кнопки
+
+        dp.add_handler(MessageHandler(Filters.regex('^(Начало работы)$'), self.hello_user))
+        dp.add_handler(MessageHandler(Filters.regex('^(Найти квартиру)$'), self.search))
+
+        self.mybot.start_polling()
+        self.mybot.idle()
 
 
+    """Разметка кнопок клавиатуры в боте"""
+    def main_keyboard(self):
+        return ReplyKeyboardMarkup([['Начало работы'], ['Найти квартиру']])
+    
 
-def main_keyboard():
-    return ReplyKeyboardMarkup([['Начало работы', 'Найти квартиру']])
+    """Функция приветсвует пользователя по имени"""
+    def hello_user(self, update, context):
+        self.name = update.message.from_user.first_name
+        update.message.reply_text(f'Привет, {self.name}! Очень рад.', reply_markup=self.main_keyboard())
 
-#-- Приветсвуем пользователя(кнопка СТАРТ)
-def hello_user(update, context):
-    name = update.message.from_user.first_name
-    update.message.reply_text(f'Привет, {name}! Очень рад.', reply_markup=main_keyboard())
 
-def search():
-    pass
-
-def main():
-    mybot = Updater('6478111175:AAHKn0haLwAn7dnCEIdDIkUxAhCSPuSmy64', use_context=True)
-    dp = mybot.dispatcher
-
-    dp.add_handler(CommandHandler('start', hello_user))
-    dp.add_handler(CommandHandler('search', search))
-    # кнопки
-
-    dp.add_handler(MessageHandler(Filters.regex('^(Начало работы)$'), hello_user))
-    dp.add_handler(MessageHandler(Filters.regex('^(Найти квартиру)$'), search))
-
-    mybot.start_polling()
-    mybot.idle()
-
-main()
+    """Пока кнопка выводит в консоль данные, получаемые от пользователя бота"""
+    def search(self, update, context):
+        print(update.message.from_user)
+        
+bot1 = Bot('6478111175:AAHKn0haLwAn7dnCEIdDIkUxAhCSPuSmy64')
+bot1.main()
