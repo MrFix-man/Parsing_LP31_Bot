4aba7cbcf632d31eb04ad2dc2ca5f0dc087ff115
Vii'Rus <cbd1986@yandex.ru>
2023-12-17 14:10:45 +0300

    убрал блок try except, сделал отдельную функцию для закрытия драйвера.

--- source/lib/parser.py
+++ source/lib/parser.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-class Parser():
+class Parser:
 
     def __init__(self, option=webdriver.ChromeOptions(), driver=webdriver.Chrome()):
         self.option = option
@@ -11,52 +11,46 @@ class Parser():
 
     def get_parsing_avito(self):
 
-        try:
-            url = "https://www.avito.ru/balashiha/kvartiry/sdam/na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg"
-            self.option.add_argument("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                                     "Chrome/120.0.0.0 Safari/537.36")
-            self.driver.get(url)
-            page_number = 1
+        url = "https://www.avito.ru/balashiha/kvartiry/sdam/na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg"
+        self.option.add_argument("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                                 "Chrome/120.0.0.0 Safari/537.36")
+        self.driver.get(url)
+        page_number = 1
 
-            for items in range(page_number):
-                offer = {}
+        for items in range(page_number):
+            offer = {}
 
-                elems = self.driver.find_elements(by=By.CSS_SELECTOR, value='div[data-marker="item"]')
+            elems = self.driver.find_elements(by=By.CSS_SELECTOR, value='div[data-marker="item"]')
 
-                for elem in elems:
-                    try:
-                        avito_id = int(elem.get_attribute("id")[1:])
-                        url_offer = elem.find_element(by=By.CSS_SELECTOR, value='a[itemprop="url"]').get_attribute(
-                            "href")
-                        item_address = elem.find_element(by=By.CSS_SELECTOR,
-                                                         value='div[data-marker="item-address"]').text.split("\n")
-                        adress = " ".join(item_address)
-                        advert = elem.text.split("\n")
-                        price = round(int(advert[1][:-10].replace(" ", "")))
-                        rooms = advert[0].split(", ")[0].split()[0]
-                        floor = int(advert[0].split(', ')[2].split('/')[0])
-                        floor_level = f"Этаж: {floor}"
+            for elem in elems:
+                avito_id = int(elem.get_attribute("id")[1:])
+                url_offer = elem.find_element(by=By.CSS_SELECTOR, value='a[itemprop="url"]').get_attribute(
+                    "href")
+                item_address = elem.find_element(by=By.CSS_SELECTOR,
+                                                 value='div[data-marker="item-address"]').text.split("\n")
+                adress = " ".join(item_address)
+                advert = elem.text.split("\n")
+                price = round(int(advert[1][:-10].replace(" ", "")))
+                rooms = advert[0].split(", ")[0].split()[0]
+                floor = int(advert[0].split(', ')[2].split('/')[0])
+                floor_level = f"Этаж: {floor}"
 
-                        offer[avito_id] = {
-                            "price": price,
-                            "adress": adress,
-                            "rooms": rooms,
-                            "floor_level": floor_level,
-                            "url_offer": url_offer
-                        }
+                offer[avito_id] = {
+                    "price": price,
+                    "adress": adress,
+                    "rooms": rooms,
+                    "floor_level": floor_level,
+                    "url_offer": url_offer
+                }
 
+        with open("first_offers.json", "w") as file:
+            json.dump(dict(sorted(offer.items(), key=lambda x: x[1]['price'])), file, indent=4, ensure_ascii=False)
 
-                    except Exception as ex:
-                        print(ex)
+        return dict(sorted(offer.items(), key=lambda x: x[1]['price']))
 
-            with open("first_offers.json", "w") as file:
-                json.dump(dict(sorted(offer.items(), key=lambda x: x[1]['price'])), file, indent=4, ensure_ascii=False)
-
-            return dict(sorted(offer.items(), key=lambda x: x[1]['price']))
-
-        finally:
-            self.driver.close()
-            self.driver.quit()
+    def _stop(self):
+        self.driver.close()
+        self.driver.quit()
 
 
 ekz_pars = Parser()
