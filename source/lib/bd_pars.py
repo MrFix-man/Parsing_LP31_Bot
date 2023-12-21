05a7ecbaca6fbb93a760d0fe56c91d6c6744bf32
mr_fix-man <reqtoun@gmail.com>
2023-12-21 10:39:40 +0200

    добавление структуры базы данных

--- source/lib/bd_pars.py
+++ source/lib/bd_pars.py
@@ -1,3 +1,11 @@
 import sqlite3
-import datetime
 
+con = sqlite3.connect('mydata.db')
+
+sql = 'CREATE TABLE ads (id INT, rooms INT, area FLOAT, price INT, address TEXT, district TEXT, floor INT, url TEXT, type TEXT)'
+
+cursor = con.cursor()
+
+cursor.execute(sql)
+
+con.close()
