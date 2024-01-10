93524b59aeb8be3fd977643edc08eb350de18220
MrFix-man <reqtoun@gmail.com>
2024-01-10 06:41:33 +0200

    добавление функционала отчистки базы

--- source/lib/db.py
+++ source/lib/db.py
@@ -4,6 +4,9 @@ from sqlalchemy import select
 
 
 def insert_pars(all_offers):
+    # очистка базы перед добавлением новой порции данных
+    db_session.query(Pars_db).delete()
+    # добавление данных
     for offers in all_offers:
         pars_db = Pars_db(avito_id=offers['avito_id'],
                           rooms=offers['rooms'],
@@ -20,5 +23,7 @@ def insert_pars(all_offers):
 
 
 def query_pars():
+    # получение всех данных
     all_data = db_session.execute(select(Pars_db)).scalars().all()
     return all_data
+    # можно попробовать реализовать получение данных по id
