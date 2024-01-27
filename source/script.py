3ee2177fcc0fdf05844d66c13b83ec65ef5fd01c
Vii'Rus <cbd1986@yandex.ru>
2024-01-27 14:25:14 +0300

    добавил сравнение того что приходит с парсеров(сайтов) с тем что лежит в базе, что бы не удалять каждый раз, а просто дополнять новыми объявлениями, переделал ключи уникальности.

--- source/script.py
+++ source/script.py
@@ -1,5 +1,4 @@
 import sqlite3
-
 from lib.parser import Parser
 from lib.db import DB
 
@@ -12,6 +11,7 @@ class DataProcessor:
         self.parser = Parser()
         self.db = DB(url=r'sqlite:///F:/Parsing_with_db/source/lib/my_database.db')
         self.database_path = database_path
+
     def push_bd_avito(self) -> None:
         return self._bd_owner_avito()
 
@@ -33,18 +33,14 @@ class DataProcessor:
 
         return data_avito
 
-
     def _get_data_from_database_drom(self):
         conn = sqlite3.connect(database_path)
         cursor = conn.cursor()
         cursor.execute("SELECT url_cars FROM pars_drom")
         data_drom = cursor.fetchall()
-
         cursor.close()
         conn.close()
-
         return data_drom
-
 
     def _see_change_avito(self) -> list[dict]:
         new_offers_avito = []
@@ -55,7 +51,6 @@ class DataProcessor:
                 new_offers_avito.append(i)
         return new_offers_avito
 
-
     def _see_change_drom(self) -> list[dict]:
         new_offers_drom = []
         for i in self._process_data_drom():
@@ -64,7 +59,6 @@ class DataProcessor:
             else:
                 new_offers_drom.append(i)
         return new_offers_drom
-
 
     def _process_data_drom(self) -> list[dict]:
         cars_list = self.parser.get_parsing_drom(self.drom_url)
@@ -79,10 +73,11 @@ class DataProcessor:
 
 if __name__ == '__main__':
     avito_url = 'https://www.avito.ru/balashiha/kvartiry/sdam-ASgBAgICAUSSA8gQ?p=5'
-    drom_url = 'https://moscow.drom.ru/auto/used/all/?unsold=1'
+    drom_url = 'https://auto.drom.ru/'
     database_path = "F:/Parsing_with_db/source/lib/my_database.db"
 
     avito = DataProcessor(avito_url=avito_url)
-    avito.push_bd_avito()
     drom = DataProcessor(drom_url=drom_url)
+
     drom.push_bd_drom()
+    avito.push_bd_avito()
