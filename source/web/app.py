ab95c9ea0b9ee6f09134624d3dae3b4194cc623f
MrFix-man <reqtoun@gmail.com>
2024-01-29 11:19:56 +0200

    Доведение разметки до ума

---
+++ source/web/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, url_for, request
+from source.lib.db import DB
+
+
+static_folder = "/static"
+template_folder = '/templates'
+app = Flask(__name__)
+db = DB("sqlite:////home/mr_fix-man/PycharmProjects/Parsing_LP31_Bot/source/my_database.db")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/drom")
+def drom():
+    auto = db.query_drom()
+    return render_template("drom.html", auto=auto, current_page='drom')
+
+
+@app.route("/avito")
+def avito():
+    home = db.query_avito()
+    return render_template("avito.html", home=home, current_page='avito')
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+
