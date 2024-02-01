c6bfaaa76ab1e45ac14c454e2b2b5dbd2b95f393
MrFix-man <reqtoun@gmail.com>
2024-02-01 14:38:00 +0200

    Доведение разметки до ума

--- source/lib/db.py
+++ source/lib/db.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.exc import OperationalError, NoSuchModuleError
-
-from source.lib.models import ParsAvito, ParsDrom
+from sqlalchemy.exc import OperationalError, NoSuchModuleError, IntegrityError
+from .models import ParsAvito, ParsDrom
 
 
 class DBError(Exception):
@@ -33,21 +32,27 @@ class DB:
     def insert_avito(self, all_offers: list[dict]) -> None:
         self._connection()
         self.create_session()
+        try:
+            for offers in all_offers:
+                existing_entry = self.session.query(ParsAvito).filter_by(avito_id=offers['avito_id']).first()
+                if existing_entry:
+                    existing_entry.rooms = offers['rooms']
+                else:
+                    pars_avito = ParsAvito(
+                        avito_id=offers['avito_id'],
+                        rooms=offers['rooms'],
+                        area=offers['area'],
+                        price=offers['price'],
+                        adress=offers['adress'],
+                        district=offers['district'],
+                        floor_level=offers['floor_level'],
+                        url_offer=offers['url_offer'],
+                        type=offers['type']
+                    )
 
-        for offers in all_offers:
-            pars_avito = ParsAvito(
-                avito_id=offers['avito_id'],
-                rooms=offers['rooms'],
-                area=offers['area'],
-                price=offers['price'],
-                adress=offers['adress'],
-                district=offers['district'],
-                floor_level=offers['floor_level'],
-                url_offer=offers['url_offer'],
-                type=offers['type']
-            )
-
-            self.session.add(pars_avito)
+                    self.session.add(pars_avito)
+        except IntegrityError:
+            self.session.rollback()
         self.session.commit()
         self.session.close()
 
@@ -56,27 +61,44 @@ class DB:
         self.create_session()
         return self.session.query(ParsAvito).all()
 
+    def query_avito_for_bot(self, count=0):
+        self._connection()
+        self.create_session()
+        if count:
+            return self.session.query(ParsAvito).limit(count)
+        else:
+            return self.session.query(ParsAvito).all()
+
     def insert_drom(self, cars_list: list[dict]) -> None:
         self._connection()
         self.create_session()
-
-        for cars in cars_list:
-            pars_drom = ParsDrom(
-                url_cars=cars['url_cars'],
-                car_name=cars['car_name'],
-                car_year=cars['car_year'],
-                short_descript=cars['short_descript'],
-                price_int=cars['price_int'],
-                town=cars['town'],
-                day_of_announcement=cars['day_of_announcement'],
-                site_evaluation=cars['site_evaluation'],
-                type=cars['type']
-            )
-            self.session.add(pars_drom)
+        try:
+            for cars in cars_list:
+                existing_entry = self.session.query(ParsDrom).filter_by(url_cars=cars['url_cars']).first()
+                if existing_entry:
+                    existing_entry.rooms = cars['url_cars']
+                else:
+                    pars_drom = ParsDrom(
+                        url_cars=cars['url_cars'],
+                        car_name=cars['car_name'],
+                        car_year=cars['car_year'],
+                        short_descript=cars['short_descript'],
+                        price_int=cars['price_int'],
+                        town=cars['town'],
+                        day_of_announcement=cars['day_of_announcement'],
+                        site_evaluation=cars['site_evaluation'],
+                        type=cars['type']
+                    )
+                    self.session.add(pars_drom)
+        except IntegrityError:
+            self.session.rollback()
         self.session.commit()
         self.session.close()
 
-    def query_drom(self):
+    def query_drom_for_bot(self, count=0):
         self._connection()
         self.create_session()
-        return self.session.query(ParsDrom).all()
+        if count:
+            return self.session.query(ParsDrom).limit(count)
+        else:
+            return self.session.query(ParsDrom).all()
